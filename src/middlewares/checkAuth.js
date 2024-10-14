3e57986679a152dd139b91d23834e5b75145830b
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-14 22:24:48 +0700

    tao ra 2 function checkOwner va checkCus

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -1,9 +1,9 @@
 const ownerModel = require("../models/ownerModel");
+const customerModel = require("../models/customerModel");
 const { responseReturn } = require("../utils/response");
-const jwt = require("jsonwebtoken");
 
 
-module.exports.checkAuth = async (req, res, next) => {
+module.exports.checkAuthOwner = async (req, res, next) => {
   try {
     const owner = await ownerModel.findById(req.id).select("-password");
     if(!owner) {
@@ -14,36 +14,14 @@ module.exports.checkAuth = async (req, res, next) => {
     console.log(error.message);
   }
 }
-
-// module.exports.checkAuth = async (req, res, next) => {
-//   const accessToken =
-//     req.cookies.accessToken ||
-//     (req.headers.authorization && req.headers.authorization.split(" ")[1]);
-//   if (!accessToken) {
-//     return res.status(401).json({ message: "You are not authenticated" });
-//   } else {
-//     try {
-//       const decodeAccessToken = jwt.verify(
-//         accessToken,
-//         process.env.SECRET_ACCESS_TOKEN
-//       );
-//       req.accessToken = { value: accessToken, exp: decodeAccessToken.exp };
-//       req.id = decodeAccessToken.id;
-//       next();
-//     } catch (error) {
-//       if (error instanceof jwt.TokenExpiredError) {
-//         responseReturn(res, 401, {
-//           message: "Access token expired",
-//           code: "AccessTokenExpired",
-//         });
-//       } else if (error instanceof jwt.JsonWebTokenError) {
-//         responseReturn(res, 401, {
-//           message: "Invalid access token",
-//           code: "InvalidAccessToken",
-//         });
-//       } else {
-//         responseReturn(res, 500, { message: error.message });
-//       }
-//     }
-//   }
-// };
+module.exports.checkAuthCustomer = async (req, res, next) => {
+  try {
+    const customer = await customerModel.findById(req.id).select("-password");
+    if(!customer) {
+      return responseReturn(res, 404, { message: "Customer not found" });
+    }
+    responseReturn(res, 200, { customer });
+  } catch (error) {
+    console.log(error.message);
+  }
+}

aade75b1e184bd9fedbe4404bd3347fba9f6f8be
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-10-14 17:19:18 +0700

    dieu chinh validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,13 +1,18 @@
 const joi = require("joi");
 
 const userValidate = (data) => {
-    const userSchema = joi.object({
-        email: joi.string().pattern(new RegExp('gmail.com$')).email().lowercase().required(),
-        password: joi.string().min(6).max(32).required(),
-    })
-    return userSchema.validate(data);
-}
+  const userSchema = joi.object({
+    email: joi
+      .string()
+      .pattern(new RegExp("(gmail.com$|dtu.edu.vn$)"))
+      .email()
+      .lowercase()
+      .required(),
+    password: joi.string().min(6).max(32).required(),
+  });
+  return userSchema.validate(data);
+};
 
 module.exports = {
-    userValidate
-}
+  userValidate,
+};

b77ba12b504f0336b1583c6e1da5e96d9652e3a6
nguyenduykiet72@gmnail.com <nguyenduykiet72@gmail.com>
2024-11-10 19:29:36 +0700

    them get active va deactive owner

--- src/controllers/ownerController.js
+++ src/controllers/ownerController.js
@@ -1,6 +1,6 @@
 "use strict";
 const ownerModel = require("../models/ownerModel");
-const bookingModel = require("../models/bookingModel")
+const bookingModel = require("../models/bookingModel");
 const { responseReturn } = require("../utils/response");
 const { post } = require("../models/postModel");
 
@@ -52,7 +52,42 @@ const get_owner_request = async (req, res) => {
   }
 };
 
-const updateBookingStatus = async (req, res) => {
+const update_owner_status = async (req, res) => {
+  const { ownerId, status } = req.body;
+  try {
+    await ownerModel.findByIdAndUpdate(ownerId, { status });
+    const owner = await ownerModel.findById(ownerId);
+    responseReturn(res, 200, {
+      owner,
+      message: "Update Status Owner Successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    responseReturn(res, 500, { error: error.message });
+  }
+};
+
+const get_active_owner = async (req, res) => {
+  try {
+    const owner = await ownerModel.find({ status: "active" });
+    responseReturn(res, 200, { owner });
+  } catch (error) {
+    console.log(error.message);
+    responseReturn(res, 500, { error: error.message });
+  }
+};
+
+const get_deactive_owner = async (req, res) => {
+  try {
+    const owner = await ownerModel.find({ status: "deactive" });
+    responseReturn(res, 200, { owner });
+  } catch (error) {
+    console.log(error.message);
+    responseReturn(res, 500, { error: error.message });
+  }
+};
+
+const update_booking_status = async (req, res) => {
   const { bookingId, status } = req.body;
   try {
     await bookingModel.findByIdAndUpdate(bookingId, { status });
@@ -76,5 +111,8 @@ module.exports = {
   add_owner_profile,
   get_owner,
   get_owner_request,
-  updateBookingStatus
+  get_active_owner,
+  get_deactive_owner,
+  update_owner_status,
+  update_booking_status,
 };
--- src/routes/ownerRoutes.js
+++ src/routes/ownerRoutes.js
@@ -4,8 +4,17 @@ const { verifyToken } = require("../middlewares/verifyToken");
 const router = require("express").Router();
 
 router.get("/get-owner/:ownerId", verifyToken, ownerController.get_owner);
+
 router.get("/get-owner-request", verifyToken, ownerController.get_owner_request);
+
+router.post("/update-owner-status", verifyToken, ownerController.update_owner_status);
+
+router.get("/get-active-owner", verifyToken, ownerController.get_active_owner);
+
+router.get("/get-deactive-owner", verifyToken, ownerController.get_deactive_owner);
+
 router.post("/add-profile-info",verifyToken,ownerController.add_owner_profile);
-router.post("/update-booking-request",verifyToken, ownerController.updateBookingStatus);
+
+router.post("/update-booking-request",verifyToken, ownerController.update_booking_status);
 
 module.exports = router;

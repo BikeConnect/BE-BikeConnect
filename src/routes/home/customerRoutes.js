619a32479b9a22a7272be5e49b68ee7861bb82e1
truongsocute <ngoctruong180603@gmail.com>
2024-12-24 18:08:22 +0700

    Add ORD Authentication

--- src/routes/home/customerRoutes.js
+++ src/routes/home/customerRoutes.js
@@ -64,8 +64,16 @@ router.get(
   customerController.analyzeIdentityCard
 );
 
-router.put("/customer-change-password", verifyToken, customerController.customer_change_password);
+router.put(
+  "/customer-change-password",
+  verifyToken,
+  customerController.customer_change_password
+);
 
-router.get("/customer/vehicle-history", verifyToken, customerController.get_customer_booking_history);
+router.get(
+  "/customer/vehicle-history",
+  verifyToken,
+  customerController.get_customer_booking_history
+);
 
 module.exports = router;
